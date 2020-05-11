9577982c846712abf03cedbc1bb10385f615a63b
Fauqhi Fikriansyah <m.fauqhi@gmail.com>
2020-05-11 14:07:14 +0800

    add authenticate

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:bajuku_app/screens/authenticate/authenticate.dart';
+import 'package:bajuku_app/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
