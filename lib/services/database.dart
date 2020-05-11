68a1751956a738d40dcf517d172208911f565c73
Fauqhi Fikriansyah <m.fauqhi@gmail.com>
2020-05-12 00:05:59 +0800

    add database.dart

---
+++ lib/services/database.dart
@@ -0,0 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  // Collection reference
+  final CollectionReference digiDrobeCollection = Firestore.instance.collection('digiDrobes');
+
+  // Future updateUserData(String baju, String )
+
+}
