b43dcf392a2aac059d5b1ad9b9e4ae1436c234d3
Fauqhi Fikriansyah <m.fauqhi@gmail.com>
2020-05-11 15:59:26 +0800

    Authentication

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,15 +1,30 @@
+import 'package:bajuku_app/services/auth.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
+class Home extends StatelessWidget {
 
-class _HomeState extends State<Home> {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Home'),
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('Brew Crew'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('logout'),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,20 @@
+import 'package:bajuku_app/models/user.dart';
 import 'package:bajuku_app/screens/authenticate/authenticate.dart';
 import 'package:bajuku_app/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Home();
+
+    final user = Provider.of<User>(context);
+    
+    // return either the Home or Authenticate widget
+    if (user == null){
+      return Authenticate();
+    } else {
+      return Home();
+    }
   }
 }
