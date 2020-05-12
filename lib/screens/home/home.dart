b5488b124db74636643cb984e0a5376cec261abd
rafialdy <64399593+RafialdyMario@users.noreply.github.com>
2020-05-12 23:02:15 +0800

    `lease enter the commit message for your changes. Lines starting

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -28,6 +28,22 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
+      body: GridView.count(
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 2,
+          // Generate 100 widgets that display their index in the List.
+          children: List.generate(6, (index) {
+          return Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Image.network(
+                    'https://assets.supremenewyork.com/184848/ma/36BcflsTxto.jpg'
+              ),
+            ),
+          );
+        }),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
 
