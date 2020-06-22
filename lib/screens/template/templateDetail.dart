ceccc29bab6e72387a09690bfe638da6ca42acf7
Fauqhi Fikriansyah <m.fauqhi@gmail.com>
2020-06-23 05:55:56 +0800

    update template detail using myscaffold

--- lib/screens/template/templateDetail.dart
+++ lib/screens/template/templateDetail.dart
@@ -1,6 +1,5 @@
-import 'package:bajuku_app/screens/template/templateCategories.dart';
+import 'package:bajuku_app/screens/page/scaffold/myscaffold.dart';
 import 'package:bajuku_app/services/database.dart';
-import 'package:bajuku_app/test.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
@@ -21,231 +20,193 @@ class _TemplateDetailState extends State<TemplateDetail> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            Container(
-              child: SliverAppBar(
-                centerTitle: true,
-                leading: IconButton(
-                  icon: Icon(Icons.arrow_back),
-                  color: Hexcolor('#3F4D55'),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                    Navigator.push(
-                        context,
-                        new MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                new TemplateCategories(
-                                    categories: widget.categories)));
-                  },
-                ),
-                iconTheme: IconThemeData(
-                  color: Hexcolor('#3F4D55'),
-                ),
-                backgroundColor: Hexcolor('FBFBFB'),
-                expandedHeight: 50.0,
-                floating: false,
-                pinned: false,
-                flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  title: Text(
-                    'Clothing Detail',
-                    style: TextStyle(
-                      color: Hexcolor('#3F4D55'),
+    return MyScaffold(
+      title: 'Clothing Detail',
+      headerWidget: [
+        
+      ],
+      body: buildBody(),
+    );
+  }
+
+  Widget buildBody() {
+    return SingleChildScrollView(
+      child: FutureBuilder(
+        future: DatabaseService().getClothes(widget.categories),
+        builder: (_, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text('');
+          } else {
+            Timestamp t =
+                snapshot.data.documents[widget.idx].data['dateBought'];
+            DateTime d = t.toDate();
+            var format = new DateFormat('dd MMMM yyyy');
+            String date = format.format(d);
+            url = snapshot.data.documents[widget.idx].data['url'];
+            return Container(
+              color: Hexcolor('#FBFBFB'),
+              child: Column(
+                children: <Widget>[
+                  // Edit button
+                  Container(
+                    margin: EdgeInsets.only(left: 330),
+                    child: IconButton(
+                        icon: new Image.asset('assets/images/edit.png'),
+                        onPressed: () {}),
+                  ),
+                  // Image of Cloth detail
+                  Container(
+                    height: 300,
+                    width: 400,
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                    child: ClipRRect(
+                      child: Card(
+                        child: Image.network(
+                          snapshot.data.documents[widget.idx].data['image'],
+                          fit: BoxFit.fitWidth,
+                        ),
+                        elevation: 2.0,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
                     ),
                   ),
-                ),
-              ),
-            ),
-          ];
-        },
-        body: SingleChildScrollView(
-          child: FutureBuilder(
-            future: DatabaseService().getClothes(widget.categories),
-            builder: (_, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Text('');
-              } else {
-                Timestamp t =
-                    snapshot.data.documents[widget.idx].data['dateBought'];
-                DateTime d = t.toDate();
-                var format = new DateFormat('dd MMMM yyyy');
-                String date = format.format(d);
-                url = snapshot.data.documents[widget.idx].data['url'];
-                return Container(
-                  color: Hexcolor('#FBFBFB'),
-                  child: Column(
-                    children: <Widget>[
-                      // Edit button
-                      Container(
-                        margin: EdgeInsets.only(left: 330),
-                        child: IconButton(
-                            icon: new Image.asset('assets/images/edit.png'),
-                            onPressed: () {}),
-                      ),
-                      // Image of Cloth detail
-                      Container(
-                        height: 300,
-                        width: 400,
-                        padding: EdgeInsets.only(left: 20, right: 20),
-                        child: ClipRRect(
-                          child: Card(
-                            child: Image.network(
-                              snapshot.data.documents[widget.idx].data['image'],
-                              fit: BoxFit.fitWidth,
+                  // Name of cloth detail and total of worns and last worns
+                  Container(
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          width: 355,
+                          padding: EdgeInsets.only(top: 10),
+                          child: Text(
+                            snapshot
+                                .data.documents[widget.idx].data['clothName'],
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                              color: Hexcolor('#3F4D55'),
+                              letterSpacing: 1.0,
+                              fontStyle: FontStyle.normal,
                             ),
-                            elevation: 2.0,
                           ),
-                          borderRadius: BorderRadius.circular(15),
                         ),
-                      ),
-                      // Name of cloth detail and total of worns and last worns
-                      Container(
-                        child: Column(
+                        Row(
                           children: <Widget>[
                             Container(
-                              width: 355,
-                              padding: EdgeInsets.only(top: 10),
+                              width: 200,
+                              padding: EdgeInsets.only(left: 30.0, top: 10.0),
                               child: Text(
-                                snapshot.data.documents[widget.idx]
-                                    .data['clothName'],
+                                snapshot.data.documents[widget.idx].data['worn']
+                                        .toString() +
+                                    ' times worn',
                                 style: TextStyle(
                                   fontSize: 16.0,
-                                  fontWeight: FontWeight.bold,
-                                  color: Hexcolor('#3F4D55'),
-                                  letterSpacing: 1.0,
+                                  fontWeight: FontWeight.normal,
+                                  color: Hexcolor('#859289'),
                                   fontStyle: FontStyle.normal,
                                 ),
                               ),
                             ),
-                            Row(
-                              children: <Widget>[
-                                Container(
-                                  width: 200,
-                                  padding:
-                                      EdgeInsets.only(left: 30.0, top: 10.0),
-                                  child: Text(
-                                    snapshot.data.documents[widget.idx]
-                                            .data['worn']
-                                            .toString() +
-                                        ' times worn',
-                                    style: TextStyle(
-                                      fontSize: 16.0,
-                                      fontWeight: FontWeight.normal,
-                                      color: Hexcolor('#859289'),
-                                      fontStyle: FontStyle.normal,
-                                    ),
-                                  ),
+                            Container(
+                              width: 185,
+                              padding: EdgeInsets.only(top: 10.0),
+                              child: Text(
+                                'Last worn ' +
+                                    snapshot
+                                        .data.documents[widget.idx].data['worn']
+                                        .toString() +
+                                    ' days ago',
+                                textAlign: TextAlign.right,
+                                style: TextStyle(
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.normal,
+                                  color: Hexcolor('#859289'),
+                                  fontStyle: FontStyle.normal,
                                 ),
-                                Container(
-                                  width: 185,
-                                  padding: EdgeInsets.only(top: 10.0),
-                                  child: Text(
-                                    'Last worn ' +
-                                        snapshot.data.documents[widget.idx]
-                                            .data['worn']
-                                            .toString() +
-                                        ' days ago',
-                                    textAlign: TextAlign.right,
-                                    style: TextStyle(
-                                      fontSize: 12.0,
-                                      fontWeight: FontWeight.normal,
-                                      color: Hexcolor('#859289'),
-                                      fontStyle: FontStyle.normal,
-                                    ),
-                                  ),
-                                ),
-                              ],
+                              ),
                             ),
                           ],
                         ),
-                      ),
-                      // Button worns
-                      Container(
-                        padding: EdgeInsets.only(
-                            left: 8.0, right: 8.0, top: 25.0, bottom: 25.0),
-                        child: FlatButton(
-                          child: Image.asset('assets/images/wornButton.png'),
-                          onPressed: () {},
-                        ),
-                      ),
-                      _buildContainerListDark('Notes',
-                          snapshot.data.documents[widget.idx].data['notes']),
-                      _buildContainerListLight(
-                          'Fabric',
-                          snapshot.data.documents[widget.idx].data['fabric']
-                              .toString()
-                              .substring(
-                                  1,
-                                  snapshot.data.documents[widget.idx]
-                                          .data['fabric']
-                                          .toString()
-                                          .length -
-                                      1)),
-                      _buildContainerListDark('Brand',
-                          snapshot.data.documents[widget.idx].data['brand']),
-                      _buildContainerListLight('Size',
-                          snapshot.data.documents[widget.idx].data['size']),
-                      _buildContainerListDark(
-                          'Season',
-                          snapshot.data.documents[widget.idx].data['season']
-                              .toString()
-                              .substring(
-                                  1,
-                                  snapshot.data.documents[widget.idx]
-                                          .data['season']
-                                          .toString()
-                                          .length -
-                                      1)),
-                      _buildContainerListLight(
-                          'Price',
-                          '€' +
-                              snapshot.data.documents[widget.idx].data['price']
-                                  .toString()),
-                      _buildContainerListDark(
-                          'Value Cost',
-                          '€' +
-                              snapshot.data.documents[widget.idx].data['cost']
-                                  .toString()),
-                      _buildContainerListLight('Date Bought', date),
-                      _buildContainerListDarkColor(
-                          'Color',
-                          snapshot.data.documents[widget.idx].data['color']
-                              .toString()
-                              .substring(10, 16)),
-                      _buildContainerListLight('Status',
-                          snapshot.data.documents[widget.idx].data['status']),
-                      _buildContainerListDark(
-                          'Used in Outfit',
-                          snapshot
-                              .data.documents[widget.idx].data['usedInOutfit']
-                              .toString()),
-                      _buildContainerListLight(
-                          'Tags Category',
-                          snapshot.data.documents[widget.idx].data['category']
-                              .toString()
-                              .substring(
-                                  1,
-                                  snapshot.data.documents[widget.idx]
-                                          .data['category']
-                                          .toString()
-                                          .length -
-                                      1)),
-                      _buildContainerListDarkURL('URL',
-                          snapshot.data.documents[widget.idx].data['url']),
-                      SizedBox(
-                        height: 50,
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                );
-              }
-            },
-          ),
-        ),
+                  // Button worns
+                  Container(
+                    padding: EdgeInsets.only(
+                        left: 8.0, right: 8.0, top: 25.0, bottom: 25.0),
+                    child: FlatButton(
+                      child: Image.asset('assets/images/wornButton.png'),
+                      onPressed: () {},
+                    ),
+                  ),
+                  _buildContainerListDark('Notes',
+                      snapshot.data.documents[widget.idx].data['notes']),
+                  _buildContainerListLight(
+                      'Fabric',
+                      snapshot.data.documents[widget.idx].data['fabric']
+                          .toString()
+                          .substring(
+                              1,
+                              snapshot.data.documents[widget.idx].data['fabric']
+                                      .toString()
+                                      .length -
+                                  1)),
+                  _buildContainerListDark('Brand',
+                      snapshot.data.documents[widget.idx].data['brand']),
+                  _buildContainerListLight(
+                      'Size', snapshot.data.documents[widget.idx].data['size']),
+                  _buildContainerListDark(
+                      'Season',
+                      snapshot.data.documents[widget.idx].data['season']
+                          .toString()
+                          .substring(
+                              1,
+                              snapshot.data.documents[widget.idx].data['season']
+                                      .toString()
+                                      .length -
+                                  1)),
+                  _buildContainerListLight(
+                      'Price',
+                      '€' +
+                          snapshot.data.documents[widget.idx].data['price']
+                              .toString()),
+                  _buildContainerListDark(
+                      'Value Cost',
+                      '€' +
+                          snapshot.data.documents[widget.idx].data['cost']
+                              .toString()),
+                  _buildContainerListLight('Date Bought', date),
+                  _buildContainerListDarkColor(
+                      'Color',
+                      snapshot.data.documents[widget.idx].data['color']
+                          .toString()
+                          .substring(10, 16)),
+                  _buildContainerListLight('Status',
+                      snapshot.data.documents[widget.idx].data['status']),
+                  _buildContainerListDark(
+                      'Used in Outfit',
+                      snapshot.data.documents[widget.idx].data['usedInOutfit']
+                          .toString()),
+                  _buildContainerListLight(
+                      'Tags Category',
+                      snapshot.data.documents[widget.idx].data['category']
+                          .toString()
+                          .substring(
+                              1,
+                              snapshot.data.documents[widget.idx]
+                                      .data['category']
+                                      .toString()
+                                      .length -
+                                  1)),
+                  _buildContainerListDarkURL(
+                      'URL', snapshot.data.documents[widget.idx].data['url']),
+                  SizedBox(
+                    height: 50,
+                  ),
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
