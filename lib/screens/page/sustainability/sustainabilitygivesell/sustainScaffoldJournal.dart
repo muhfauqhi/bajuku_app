e8ef5517d46722287e0f17f0aacc4b7dca78f79d
muhfauqhi <m.fauqhi@gmail.com>
2020-08-18 17:03:42 +0800

    updated

--- lib/screens/page/sustainability/sustainabilitygivesell/sustainScaffoldJournal.dart
+++ lib/screens/page/sustainability/sustainabilitygivesell/sustainScaffoldJournal.dart
@@ -1,13 +1,56 @@
 import 'package:bajuku_app/models/outfit.dart';
 import 'package:bajuku_app/screens/page/scaffold/myscaffold.dart';
 import 'package:bajuku_app/screens/page/sustainability/sustainabilitygivesell/sustainabiilityJournal.dart';
+import 'package:bajuku_app/screens/template/templateDetailOutfits.dart';
 import 'package:bajuku_app/services/database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class SustainScaffoldJournal extends StatelessWidget {
   final String type;
   SustainScaffoldJournal({this.type});
+
+  final DatabaseService _databaseService = DatabaseService();
+
+  Future fetchData() async {
+    List id = [];
+    List available = [];
+    QuerySnapshot snapshot = await _databaseService.getOutfit();
+    var documents = snapshot.documents;
+    documents.forEach((e) {
+      var list = e.data['tagged'];
+      list.forEach((i) {
+        String docId =
+            i.values.toString().substring(1, i.values.toString().length - 1);
+        if (id.length == 0) {
+          id.add(docId);
+        } else if (!id.contains(docId)) {
+          id.add(docId);
+        }
+      });
+    });
+    for (var i in id) {
+      DocumentSnapshot snapshot = await _databaseService.getCloth(i);
+      var status = snapshot.data['status'];
+      if (status == 'Available') {
+        available.add(snapshot.data);
+      }
+    }
+    return available;
+  }
+
+  Future<List<Outfit>> getData() async {
+    List<Outfit> outfit = [];
+    QuerySnapshot snapshot = await _databaseService.getOutfit();
+
+    snapshot.documents.forEach((e) {
+      outfit.add(Outfit(e.data['image'], e.data['notes'], e.data['outfitName'],
+          e.data['tagged'], e.data['totalCost'], e.data['created']));
+    });
+
+    return outfit;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,31 +81,13 @@ class SustainScaffoldJournal extends StatelessWidget {
       headerWidget: [],
       body: Container(
         child: FutureBuilder(
-          future: DatabaseService().getOutfit(),
+          future: getData(),
           builder: (context, snapshot) {
-            List<Outfit> outfitList = [];
-
             if (snapshot.hasData) {
-              for (var i in snapshot.data.documents) {
-                for (var a in i.data['tagged'].values.toList()) {
-                  if (a['status'] == 'Available') {
-                    outfitList.add(
-                      Outfit(
-                        i.data['image'],
-                        i.data['notes'],
-                        i.data['outfitName'],
-                        i.data['tagged'],
-                        i.data['totalCost'],
-                        i.data['created'],
-                      ),
-                    );
-                    break;
-                  }
-                }
-              }
+              var data = snapshot.data;
               return GridView.builder(
                   shrinkWrap: true,
-                  itemCount: outfitList.length,
+                  itemCount: data.length,
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 3,
                     mainAxisSpacing: 10.0,
@@ -70,13 +95,14 @@ class SustainScaffoldJournal extends StatelessWidget {
                     childAspectRatio: 1,
                   ),
                   itemBuilder: (context, index) {
+                    String image = data[index].image;
                     return GestureDetector(
                       child: Card(
                         elevation: 2.0,
                         child: Container(
                           child: Image.network(
-                            outfitList[index].image,
-                            fit: BoxFit.fitWidth,
+                            image,
+                            fit: BoxFit.cover,
                           ),
                           decoration: BoxDecoration(
                             color: Hexcolor('#FFFFFF'),
@@ -90,11 +116,19 @@ class SustainScaffoldJournal extends StatelessWidget {
                           MaterialPageRoute(
                             builder: (BuildContext context) =>
                                 SustainDetailJournal(
-                              outfit: outfitList[index],
+                              outfit: snapshot.data[index],
                               type: type,
                             ),
                           ),
                         );
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //     builder: (context) => DetailOutfits(
+                        //       outfit: snapshot.data[index],
+                        //     ),
+                        //   ),
+                        // );
                       },
                     );
                   });
